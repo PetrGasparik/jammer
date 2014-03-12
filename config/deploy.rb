1bfcdc277ef2cdf181330ae7d090e1da65a2bbc3
Simon Detheridge <simon@widgit.com>
2014-03-12 11:53:51 +0000

    Further fixes to deployment recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,10 +4,6 @@ lock '3.1.0'
 set :application, 'Jammer'
 set :repo_url, 'https://github.com/gh2k/jammer.git'
 
-require 'whenever/capistrano'
-
-set :whenever_command, "bundle exec whenever"
-
 require 'capistrano/setup'
 require 'capistrano/deploy'
 
@@ -16,25 +12,15 @@ set :rvm_ruby_version, '2.0.0'
 SSHKit.config.command_map[:rake]  = 'bundle exec rake'
 SSHKit.config.command_map[:rails] = 'bundle exec rails'
 
-set :bundle_bins, %w{gem rake ruby}
+set :whenever_roles, %w(db)
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :bundle_bins, %w{gem rake ruby}
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/www/jammer.sd.ai/rails'
 
 # Default value for :log_level is :debug
 set :log_level, :info
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
 
@@ -49,11 +35,11 @@ namespace :deploy do
   after :publishing, :restart
 
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      within release_path do
-        execute :rake, 'cache:clear'
-      end
-    end
+    #on roles(:web), in: :groups, limit: 3, wait: 10 do
+    #  within release_path do
+    #    execute :rake, 'cache:clear'
+    #  end
+    #end
   end
 
   namespace :jammer do
