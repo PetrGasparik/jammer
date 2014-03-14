6cec52f0c75fc663a30896f40c9386ce024e2146
Simon Detheridge <simon@widgit.com>
2014-03-14 12:25:46 +0000

    Add index on alias to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140314121632) do
+ActiveRecord::Schema.define(version: 20140314122457) do
 
   create_table "users", force: true do |t|
     t.string   "alias"
@@ -28,5 +28,7 @@ ActiveRecord::Schema.define(version: 20140314121632) do
     t.integer  "overdue_count"
     t.float    "overdue_btc"
   end
+
+  add_index "users", ["alias"], name: "index_users_on_alias", using: :btree
 
 end
