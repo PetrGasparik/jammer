93c26650fcece310f7b7a5bcc6f409c21455b48e
Simon Detheridge <simon@widgit.com>
2014-03-24 11:28:39 +0000

    Reverse order of credit ratings

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,11 +36,11 @@ class User < ActiveRecord::Base
           overdue_btc = from_btc_str(overdue_text.scan(/^฿(\d+\.\d+)/).last.first)
           overdue_count = overdue_text.scan(/\((\d+)\)$/).last.first
 
-          credit_rating = {'A+' => 0, 'A' => 1, 'A-' => 2,
-                           'B+' => 3, 'B' => 4, 'B-' => 5,
-                           'C+' => 6, 'C' => 7, 'C-' => 8,
-                           'D+' => 9, 'D' => 10, 'D-' => 11,
-                           'E+' => 12, 'E' => 13, 'E-' => 14}[page.at('span.credit_label').text.gsub(/\s/, '')]
+          credit_rating = {'A+' => 14, 'A' => 13, 'A-' => 12,
+                           'B+' => 11, 'B' => 10, 'B-' => 9,
+                           'C+' => 8, 'C' => 7, 'C-' => 6,
+                           'D+' => 5, 'D' => 4, 'D-' => 3,
+                           'E+' => 2, 'E' => 1, 'E-' => 0}[page.at('span.credit_label').text.gsub(/\s/, '')]
 
           attribs = {:id => uid,
                      :alias => user_alias,
