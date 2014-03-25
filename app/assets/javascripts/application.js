579f9b49fcae330f6008c391b3e3ccdbb8e21c1d
Simon Detheridge <simon@widgit.com>
2014-03-25 21:07:04 +0000

    Add google analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,13 @@ $(function() {
     // --- Using the default options:
     $(".expand").toggler();
 });
+(function ($) {
+
+// Log all jQuery AJAX requests to Google Analytics
+    $(document).ajaxSend(function(event, xhr, settings){
+        if (typeof _gaq !== "undefined" && _gaq !== null) {
+            _gaq.push(['_trackPageview', settings.url]);
+        }
+    });
+
+})(jQuery);
